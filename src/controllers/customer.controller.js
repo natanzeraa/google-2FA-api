25def163d73b47738e58f32e16015027499f4a9a
natanzeraa <contato@natanfelipe.com.br>
2025-08-27 16:30:05 -0300

    refactor: improve formatting and logging in customer controller methods

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -3,13 +3,23 @@ import { handleResponse } from '../utils/handlers/response.handler.js'
 
 const findAll = (req, res) => handleResponse(res, customerService.findAll())
 
-const findById = (req, res) => handleResponse(res, customerService.findById(req.params.id))
+const findById = (req, res) =>
+  handleResponse(res, customerService.findById(req.params.id))
 
-const create = (req, res) => handleResponse(res, customerService.create(req.body))
+const create = (req, res) => {
+  console.log(req.body)
 
-const update = (req, res) => handleResponse(res, customerService.update({id: req.params.id, body: req.body}))
+  handleResponse(res, customerService.create({ body: req.body }))
+}
 
-const destroy = (req, res) => handleResponse(res, customerService.destroy(req.params.id))
+const update = (req, res) =>
+  handleResponse(
+    res,
+    customerService.update({ id: req.params.id, body: req.body })
+  )
+
+const destroy = (req, res) =>
+  handleResponse(res, customerService.destroy(req.params.id))
 
 export { create, destroy, findAll, findById, update }
 
