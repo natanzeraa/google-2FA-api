f5ff56bfde6d5c732f7366c22c34bcedf76aae27
natanzeraa <contato@natanfelipe.com.br>
2025-08-27 16:30:18 -0300

    fix: remove unnecessary console log and decoded assignment in checkToken middleware

--- src/middlewares/token.middleware.js
+++ src/middlewares/token.middleware.js
@@ -7,13 +7,7 @@ const checkToken = (req, res, next) => {
 
   const token = authHeader.split(' ')[1]
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-
     if (err) return res.status(403).json({ message: 'Invalid token' })
-
-    req.body = decoded
-
-    console.log(req.user)
-
     next()
   })
 }
