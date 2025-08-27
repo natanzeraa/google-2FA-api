0d102024ea5d56704696baa358b0c1fa551b6e6d
natanzeraa <contato@natanfelipe.com.br>
2025-08-27 16:15:22 -0300

    feat: add refresh token route to authentication router

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { login, signup } from '../controllers/auth.controller.js'
+import { tokenController } from '../controllers/token.controller.js'
 import loginMiddleware from '../middlewares/login.middleware.js'
 import signupMiddleware from '../middlewares/signup.middleware.js'
 
@@ -7,5 +8,6 @@ const router = express.Router()
 
 router.post('/signup', signupMiddleware, signup)
 router.post('/login', loginMiddleware, login)
+router.post('/refresh', tokenController)
 
 export default router
