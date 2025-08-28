85cb5dc114af87e4437b0bf5e81567157b6c6a89
natanzeraa <contato@natanfelipe.com.br>
2025-08-28 17:31:36 -0300

    refactor: update auth service import

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,8 @@
-import * as authService from '../services/auth.service.js'
+import * as auth from '../services/auth.service.js'
 import { handleResponse } from '../utils/handlers/response.handler.js'
 
-const login = (req, res) => handleResponse(res, authService.login(req.body))
-const signup = (req, res) => handleResponse(res, authService.signup(req.body))
+const login = (req, res) => handleResponse(res, auth.authService.login(req.body))
+const signup = (req, res) => handleResponse(res, auth.authService.signup(req.body))
 
 export { login, signup }
 
