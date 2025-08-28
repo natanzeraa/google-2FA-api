1ee9d2fc5701b0f718abcd18789ece3dfbbe1117
natanzeraa <contato@natanfelipe.com.br>
2025-08-28 17:32:40 -0300

    feat: add routes for 2FA

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import { login, signup } from '../controllers/auth.controller.js'
 import { tokenController } from '../controllers/token.controller.js'
+import { checkTOTPAuth, enableTOTPAuth } from '../controllers/totp.controller.js'
 import loginMiddleware from '../middlewares/login.middleware.js'
 import signupMiddleware from '../middlewares/signup.middleware.js'
+import { checkToken } from '../middlewares/token.middleware.js'
 
 const router = express.Router()
 
 router.post('/signup', signupMiddleware, signup)
 router.post('/login', loginMiddleware, login)
 router.post('/refresh', tokenController)
+router.post('/2fa/enable', checkToken, enableTOTPAuth)
+router.post('/2fa/check', checkToken, checkTOTPAuth)
 
 export default router
