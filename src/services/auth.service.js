5e30ba99b5322b2c760ed4070f554e0f2a96e189
natanzeraa <contato@natanfelipe.com.br>
2025-08-28 17:33:08 -0300

    fix: update error message return

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,110 +2,109 @@ import bcrypt from 'bcrypt'
 import { default as Organization } from '../models/organization.js'
 import { generateTokens } from '../utils/handlers/token.handler.js'
 import * as authMsg from '../utils/messages/auth.messages.js'
+import * as organizationMsg from '../utils/messages/organization.messages.js'
 import { loginSchema } from '../utils/validations/login.schema.js'
 import { signupSchema } from '../utils/validations/signup.schema.js'
 
-const login = async (data) => {
-  try {
-    const parsed = loginSchema.safeParse(data)
+export const authService = {
+  login: async (data) => {
+    try {
+      const parsed = loginSchema.safeParse(data)
 
-    if (!parsed.success) {
-      const errors = parsed.error.errors.map((err) => ({
-        field: err.path[0],
-        message: err.message
-      }))
-      return res.status(400).json({ errors })
+      if (!parsed.success) {
+        const errors = parsed.error.errors.map((err) => ({
+          field: err.path[0],
+          message: err.message
+        }))
+        return res.status(400).json({ errors })
+      }
+
+      const { email, password } = parsed.data
+
+      const organization = await Organization.findOne({ email })
+      if (!organization) throw new Error(organizationMsg.errorMessages.ORGANIZATION_NOT_FOUND)
+
+      const passwordsMatch = bcrypt.compare(password, organization.password)
+      if (!passwordsMatch) throw new Error(authMsg.errorMessages.INVALID_CREDENTIALS)
+
+      const { accessToken, refreshToken } = generateTokens({
+        id: organization._id
+      })
+
+      return {
+        success: true,
+        status: 200,
+        organization: {
+          _id: organization._id,
+          id: organization.id,
+          companyName: organization.company_name,
+          email: organization.email,
+          cnpj: organization.cnpj,
+          phone: organization.phone
+        },
+        accessToken,
+        refreshToken,
+        message: authMsg.successMessages.SUCCESS_LOGING_IN
+      }
+    } catch (error) {
+      throw new Error(error)
     }
+  },
 
-    const { email, password } = parsed.data
+  signup: async (data) => {
+    try {
+      const parsed = signupSchema.safeParse(data)
 
-    const organization = await Organization.findOne({ email })
-    if (!organization) throw new Error(errorMessages.ACCOUNT_NOT_FOUND)
+      if (!parsed.success) {
+        const errors = parsed.error.errors.map((err) => ({
+          field: err.path[0],
+          message: err.message
+        }))
+        return res.status(400).json({ errors })
+      }
 
-    const passwordsMatch = bcrypt.compare(password, organization.password)
-    if (!passwordsMatch) throw new Error(errorMessages.INVALID_CREDENTIALS)
+      const { companyName, email, cnpj, phone, password, twoFaEnabled } = parsed.data
 
-    const { accessToken, refreshToken } = generateTokens({
-      id: organization._id
-    })
+      const cnpjExists = await Organization.findOne({ cnpj })
+      if (cnpjExists) throw new Error(organizationMsg.errorMessages.CNPJ_IN_USE_ALREADY)
 
-    return {
-      success: true,
-      status: 200,
-      organization: {
-        _id: organization._id,
-        id: organization.id,
-        companyName: organization.company_name,
-        email: organization.email,
-        cnpj: organization.cnpj,
-        phone: organization.phone
-      },
-      accessToken,
-      refreshToken,
-      message: authMsg.successMessages.SUCCESS_LOGING_IN
+      const emailExists = await Organization.findOne({ email })
+      if (emailExists) throw new Error(authMsg.errorMessages.EMAIL_IN_USE_ALREADY)
+
+      const phoneExists = await Organization.findOne({ phone })
+      if (phoneExists) throw new Error(authMsg.errorMessages.PHONE_IN_USE_ALREADY)
+
+      const organization = new Organization({
+        company_name: companyName,
+        email,
+        cnpj,
+        phone,
+        password,
+        two_fa_enabled: twoFaEnabled
+      })
+      await organization.save()
+
+      const { accessToken, refreshToken } = generateTokens({
+        id: organization._id
+      })
+
+      return {
+        success: true,
+        status: 201,
+        organization: {
+          _id: organization._id,
+          id: organization.id,
+          company_name: organization.company_name,
+          email: organization.email,
+          cnpj: organization.cnpj,
+          phone: organization.phone
+        },
+        accessToken,
+        refreshToken,
+        message: authMsg.successMessages.SUCCESS_CREATING_ACCOUNT
+      }
+    } catch (error) {
+      throw new Error(error)
     }
-  } catch (error) {
-    console.log('Erro ao tentar logar: ', error.message)
-    throw new Error(error.message)
   }
 }
-
-const signup = async (data) => {
-  try {
-    const parsed = signupSchema.safeParse(data)
-
-    if (!parsed.success) {
-      const errors = parsed.error.errors.map((err) => ({
-        field: err.path[0],
-        message: err.message
-      }))
-      return res.status(400).json({ errors })
-    }
-
-    const { companyName, email, cnpj, phone, password } = parsed.data
-
-    const cnpjExists = await Organization.findOne({ cnpj })
-    if (cnpjExists) throw new Error(errorMessages.PHONE_IN_USE_ALREADY)
-
-    const emailExists = await Organization.findOne({ email })
-    if (emailExists) throw new Error(errorMessages.EMAIL_IN_USE_ALREADY)
-
-    const phoneExists = await Organization.findOne({ phone })
-    if (phoneExists) throw new Error(errorMessages.PHONE_IN_USE_ALREADY)
-
-    const organization = new Organization({
-      company_name: companyName,
-      email,
-      cnpj,
-      phone,
-      password
-    })
-    await organization.save()
-
-    const { accessToken, refreshToken } = generateTokens({
-      id: organization._id
-    })
-
-    return {
-      success: true,
-      status: 201,
-      organization: {
-        _id: organization._id,
-        id: organization.id,
-        company_name: organization.company_name,
-        email: organization.email,
-        cnpj: organization.cnpj,
-        phone: organization.phone
-      },
-      accessToken,
-      refreshToken,
-      message: authMsg.successMessages.SUCCESS_CREATING_ACCOUNT
-    }
-  } catch (error) {
-    console.log('(auth_service.js) | Erro ao criar conta: ', error.message)
-    throw new Error(error.message)
-  }
-}
-
-export { login, signup }
-
