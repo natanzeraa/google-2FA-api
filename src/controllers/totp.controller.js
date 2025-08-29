efa011c3446140f336c1ca366a056667edac29f2
natanzeraa <contato@natanfelipe.com.br>
2025-08-29 14:42:35 -0300

    feat: add controller for TOTPAuth

--- src/controllers/totp.controller.js
+++ src/controllers/totp.controller.js
@@ -1,24 +1,13 @@
 import { otpAuthService } from '../services/otpauth.service.js'
 import { handleResponse } from '../utils/handlers/response.handler.js'
 
-const enableTOTPAuth = (req, res) =>
-  handleResponse(
-    res,
-    otpAuthService.enableTOTPAuth({
-      companyName: req.body.companyName,
-      email: req.body.email
-    })
-  )
+const enableTOTPAuth = (req, res) => {
+  handleResponse(res, otpAuthService.enableTOTPAuth(req.body))
+}
 
-const checkTOTPAuth = (req, res) =>
-  handleResponse(
-    res,
-    otpAuthService.checkTOTPAuth({
-      token: req.body.token,
-      companyName: req.body.companyName,
-      email: req.body.email
-    })
-  )
+const checkTOTPAuth = (req, res) => {
+  handleResponse(res, otpAuthService.checkTOTPAuth(req.body))
+}
 
 export { checkTOTPAuth, enableTOTPAuth }
 
