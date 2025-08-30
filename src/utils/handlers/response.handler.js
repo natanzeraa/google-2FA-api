86ae7ab73a0e1f6f97dceb60f424c0f1095d6a61
natanzeraa <contato@natanfelipe.com.br>
2025-08-29 22:37:03 -0300

    fix: adjust handle response middleware for returning the correct status code and message to the client

--- src/utils/handlers/response.handler.js
+++ src/utils/handlers/response.handler.js
@@ -3,9 +3,9 @@ export const handleResponse = async (res, serviceCall) => {
     const response = await serviceCall
     return res.status(response.status).json(response)
   } catch (error) {
-    return res.status(500).json({
+    return res.status(error.status || 500).json({
       success: false,
-      status: 500,
+      status: error.status || 500,
       message: error.message || 'Erro interno no servidor'
     })
   }
