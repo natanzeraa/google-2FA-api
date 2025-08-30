4a478cb39cf92d10a41dc98ef269172f32786634
natanzeraa <contato@natanfelipe.com.br>
2025-08-29 22:34:36 -0300

    feat: add route for disabling 2FA

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import { login, signup } from '../controllers/auth.controller.js'
 import { tokenController } from '../controllers/token.controller.js'
-import { checkTOTPAuth, enableTOTPAuth } from '../controllers/totp.controller.js'
+import {
+  checkTOTPAuth,
+  disableTOTPAuth,
+  enableTOTPAuth
+} from '../controllers/totp.controller.js'
 import loginMiddleware from '../middlewares/login.middleware.js'
 import signupMiddleware from '../middlewares/signup.middleware.js'
 import { checkToken } from '../middlewares/token.middleware.js'
@@ -12,6 +16,7 @@ router.post('/signup', signupMiddleware, signup)
 router.post('/login', loginMiddleware, login)
 router.post('/refresh', tokenController)
 router.post('/2fa/enable', checkToken, enableTOTPAuth)
+router.post('/2fa/disable', checkToken, disableTOTPAuth)
 router.post('/2fa/check', checkToken, checkTOTPAuth)
 
 export default router
