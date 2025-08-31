f9114afa7636af473e5ecf98790b191769299393
natanzeraa <contato@natanfelipe.com.br>
2025-08-31 10:10:18 -0300

    feat: add user schema for data model objects

---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
+import { v4 as uuidv4 } from 'uuid'
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuidv4,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  two_fa_enabled: {
+    type: Boolean,
+    required: true
+  },
+  totp_secret: {
+    type: String,
+    required: false,
+    length: 20
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+export default mongoose.model('User', userSchema)
