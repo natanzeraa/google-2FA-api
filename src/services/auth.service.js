4079b65eb838840c9574fed89b687c0b756d56ae
natanzeraa <contato@natanfelipe.com.br>
2025-08-31 10:48:27 -0300

    refactor: change auth service to use data from user instead organization

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,12 @@
 import bcrypt from 'bcrypt'
-import { default as Organization } from '../models/organization.js'
+import { default as User } from '../models/user.js'
 import { AuthException } from '../utils/exceptions/auth.exceptions.js'
 import { requiredFieldsHandler } from '../utils/handlers/requiredFileds.handler.js'
 import { generateTokens } from '../utils/handlers/token.handler.js'
 import * as authMsg from '../utils/messages/auth.messages.js'
-import * as organizationMsg from '../utils/messages/organization.messages.js'
-import { loginSchema } from '../utils/validations/login.schema.js'
-import { signupSchema } from '../utils/validations/signup.schema.js'
+import * as userMsg from '../utils/messages/user.messages.js'
+import { loginSchema } from '../utils/schemas/login.schema.js'
+import { signupSchema } from '../utils/schemas/signup.schema.js'
 import { otpAuthService } from './otpauth.service.js'
 
 export const authService = {
@@ -16,24 +16,21 @@ export const authService = {
 
       const { email, password } = parsed.data
 
-      const organization = await Organization.findOne({ email })
-      if (!organization) {
-        throw new AuthException(
-          organizationMsg.errorMessages.ORGANIZATION_NOT_FOUND,
-          404
-        )
+      const user = await User.findOne({ email })
+      if (!user) {
+        throw new AuthException(userMsg.errorMessages.USER_NOT_FOUND, 404)
       }
 
-      const passwordsMatch = bcrypt.compare(password, organization.password)
+      const passwordsMatch = bcrypt.compare(password, user.password)
       if (!passwordsMatch) {
         throw new AuthException(authMsg.errorMessages.INVALID_CREDENTIALS, 400)
       }
 
-      if (organization.two_fa_enabled) {
+      if (user.two_fa_enabled) {
         const result = await otpAuthService.checkTOTPAuth({
           token: data.token,
-          companyName: organization.company_name,
-          email: organization.email
+          name: user.name,
+          email: user.email
         })
 
         if (!result.success) {
@@ -42,20 +39,20 @@ export const authService = {
       }
 
       const { accessToken, refreshToken } = generateTokens({
-        id: organization._id
+        id: user._id
       })
 
       return {
         success: true,
         status: 200,
-        organization: {
-          _id: organization._id,
-          id: organization.id,
-          companyName: organization.company_name,
-          email: organization.email,
-          cnpj: organization.cnpj,
-          phone: organization.phone,
-          twoFaEnabled: organization.two_fa_enabled
+        user: {
+          _id: user._id,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          cnpj: user.cnpj,
+          phone: user.phone,
+          twoFaEnabled: user.two_fa_enabled
         },
         accessToken,
         refreshToken,
@@ -70,48 +67,34 @@ export const authService = {
     try {
       const parsed = requiredFieldsHandler({ schema: signupSchema, data: data })
 
-      const { companyName, email, cnpj, phone, password, twoFaEnabled } =
-        parsed.data
+      const { name, email, password, twoFaEnabled } = parsed.data
 
-      const cnpjExists = await Organization.findOne({ cnpj })
-      if (cnpjExists)
-        throw new AuthException(
-          organizationMsg.errorMessages.CNPJ_IN_USE_ALREADY,
-          409
-        )
-
-      const emailExists = await Organization.findOne({ email })
+      const emailExists = await User.findOne({ email })
       if (emailExists)
         throw new AuthException(authMsg.errorMessages.EMAIL_IN_USE_ALREADY)
 
-      const phoneExists = await Organization.findOne({ phone })
-      if (phoneExists)
-        throw new AuthException(authMsg.errorMessages.PHONE_IN_USE_ALREADY)
-
-      const organization = new Organization({
-        company_name: companyName,
+      const user = new User({
+        name,
         email,
-        cnpj,
-        phone,
         password,
         two_fa_enabled: twoFaEnabled
       })
-      await organization.save()
+
+      await user.save()
 
       const { accessToken, refreshToken } = generateTokens({
-        id: organization._id
+        id: user._id
       })
 
       return {
         success: true,
         status: 201,
-        organization: {
-          _id: organization._id,
-          id: organization.id,
-          company_name: organization.company_name,
-          email: organization.email,
-          cnpj: organization.cnpj,
-          phone: organization.phone
+        user: {
+          _id: user._id,
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          twoFaEnabled: user.two_fa_enabled
         },
         accessToken,
         refreshToken,
